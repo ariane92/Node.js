a46737e5cc8546709ff380b7c3885e32dd49a71b
Ariane Mateus <ariane.ccomp@gmail.com>
2020-04-14 19:08:56 -0300

    route post

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,13 @@
+import { uuid } from 'uuidv4';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+// import transactionRouter from '../routes/transaction.routes';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +15,19 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    const receberiddarota = uuid();
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    if (receberiddarota) {
+      throw Error('This transaction is alread');
+    }
+
+    return transaction;
   }
 }
 
